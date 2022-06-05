dd3db07b267c621b18b89f85abb4ddc8e1813d49
sptungG <ngoviettung154@gmail.com>
2022-06-05 21:25:56 +0700

    update

---
+++ cppweb/crow_demo_01/build/CMakeFiles/crow_demo_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crow_demo_01.dir/main.cpp.o"
+  "crow_demo_01.pdb"
+  "crow_demo_01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/crow_demo_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
